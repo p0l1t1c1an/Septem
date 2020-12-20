e6e976d2de6e7937996aec30dd0712269400b039
p0l1t1c1an <boicken19@gmail.com>
2020-12-20 06:46:46 -0600

    Added system to proc name (Freebsd)

--- src/config.rs
+++ src/config.rs
@@ -1,77 +1,73 @@
 
-pub use self::config::*;
+use std::env;
+use std::time::Duration;
+use std::error::Error;
 
-mod config {
-    use std::env;
-    use std::time::Duration;
-    use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+
+pub enum ConfigError {
+    G,
+}
+
+pub struct Config {
+    config_file : String,
+    shared_dir : String,
+    delay : Duration,
+    whitelist : Vec<String>,
+    use_PID : bool,
+    use_name : bool,
+}
+
+impl Config {
+    pub fn new(c : String, s : String) -> Result<Config, ConfigError> {
+
+    }
+
+    pub fn config_file<'a>(&'a self) -> &'a String {
+        &self.config_file
+    }
     
-    use std::fs::File;
-    use std::io::prelude::*;
-    use std::io::{self, BufReader};
-
-    pub enum ConfigError {
-        G,
+    pub fn shared_dir<'a>(&'a self) -> &'a String {
+        &self.shared_dir
     }
 
-    pub struct Config {
-        config_file : String,
-        shared_dir : String,
-        delay : Duration,
-        whitelist : Vec<String>,
-        use_PID : bool,
-        use_name : bool,
+    pub fn delay(&self) -> Duration {
+        self.delay
+    }
+    
+    pub fn whitelist<'a>(&'a self) -> &'a Vec<String> {
+        &self.whitelist
     }
 
-    impl Config {
-        pub fn new(c : String, s : String) -> Result<Config, ConfigError> {
-
-        }
-
-        pub fn config_file<'a>(&'a self) -> &'a String {
-            &self.config_file
-        }
-        
-        pub fn shared_dir<'a>(&'a self) -> &'a String {
-            &self.shared_dir
-        }
-
-        pub fn delay(&self) -> Duration {
-            self.delay
-        }
-        
-        pub fn whitelist<'a>(&'a self) -> &'a Vec<String> {
-            &self.whitelist
-        }
-
-        pub fn use_PID(&self) -> bool {
-            self.use_PID
-        }
-
-        pub fn use_name(&self) -> bool {
-            self.use_name
-        }
+    pub fn use_PID(&self) -> bool {
+        self.use_PID
     }
 
-    #[macro_export]
-    macro_rules! config {
-        (config:$c:expr, share:$s:expr) => { // Both Strings
-            Config::new($c, $s)
-        }
+    pub fn use_name(&self) -> bool {
+        self.use_name
+    }
+}
 
-        (config:$c:expr) => {
-            let home = env::var("HOME").unwrap();
-            Config::new($c, home + "/.local/share/Septem/")
-        }
- 
-        (share:$s:expr) => {
-            let home = env::var("HOME").unwrap();
-            Config::new(home + "/.config/Septem/septem.toml", $s)
-        }
+#[macro_export]
+macro_rules! config {
+    (config:$c:expr, share:$s:expr) => { // Both Strings
+        Config::new($c, $s)
+    }
 
-        () => {
-            let home = env::var("HOME").unwrap();
-            Config::new(home + "/.config/Septem/septem.toml", home + "/.local/share/Septem/")
-        }
+    (config:$c:expr) => {
+        let home = env::var("HOME").unwrap();
+        Config::new($c, home + "/.local/share/Septem/")
+    }
+
+    (share:$s:expr) => {
+        let home = env::var("HOME").unwrap();
+        Config::new(home + "/.config/Septem/septem.toml", $s)
+    }
+
+    () => {
+        let home = env::var("HOME").unwrap();
+        Config::new(home + "/.config/Septem/septem.toml", home + "/.local/share/Septem/")
     }
 }
---
+++ src/process/process.rs
@@ -0,0 +1,44 @@
+
+use libc::{c_int, c_char, pid_t, free, strlen};
+
+extern "C" {
+    fn proc_name(name_ptr : *mut c_char, pid : pid_t) -> c_int;
+}
+
+pub enum ProcessError {
+    KInfoError,
+    StrMallocError,
+}
+
+pub struct Process {
+    pub pid : pid_t,
+    pub name : String
+}
+
+impl Process {
+    unsafe fn set_name(&mut self, ptr : *const c_char){
+        for i in 0..strlen(ptr) {
+            self.name.push(*ptr.offset(i as isize) as u8 as char);
+        }
+    }
+
+    pub fn new(p : pid_t) -> Result<Process, ProcessError> {
+        let proc = Process {
+            pid : p,
+            name : String::new()
+        };
+        unsafe {
+            let name_ptr : *mut c_char;
+            let err = proc_name(name_ptr, p);
+            match err {
+                0 => proc.set_name(name_ptr),
+                1 => return Err(ProcessError::StrMallocError),
+                2 => return Err(ProcessError::KInfoError),
+            }
+            free(name_ptr);
+        }
+
+        Ok(proc)
+
+    }
+}
