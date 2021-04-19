6c2b2db2504e7a2ea6b8778209c77004ecf00e16
p0l1t1c1an <boicken19@gmail.com>
2021-04-19 06:34:05 -0500

    Added advanced config options

--- src/application.rs
+++ src/application.rs
@@ -1,5 +1,5 @@
 
-//mod config;
+mod config;
 pub mod event_handler;
 mod process;
 //mod recorder; 
--- src/application/config.rs
+++ src/application/config.rs
@@ -13,7 +13,7 @@ const DEFAULT_SHARE : &str = "/.local/share/Septem/";
 const DEFAULT_CONFIG : &str = "/.config/Septem/septem.toml";
 
 #[derive(Error, Debug)]
-enum ConfigError {
+pub enum ConfigError {
     #[error("Failed to retrieve env HOME:\n{0}")]
     EnvError(#[from] env::VarError),
     
@@ -28,33 +28,86 @@ enum ConfigError {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Config {
-    shared_dir : String,
-    proc_names : Vec<String>,
-    dates : Vec<Datetime>,
-    start_hour : u8,
+pub struct EventHandlerConfig {
+    min_focus_time : u8,
+}
+
+impl Default for EventHandlerConfig {
+    fn default() -> Self {
+        Self { min_focus_time : 10 }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RecorderConfig {
+    productive : Vec<String>,
+    unproductive : Vec<String>,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct MonthWeekDay {
+    month : u8,
+    week : u8, 
+    day : u8,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct MonthDay {
+    month : u8,
+    day : u8,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct WeekDay {
+    week : u8,
+    day : u8,
+}
+
+#[derive(Clone, Deserialize, Debug, Default)]
+pub struct DateTimeConfig {
+    month_week_days : Vec<MonthWeekDay>,
+    month_days : Vec<MonthDay>,
+    week_days : Vec<WeekDay>,
+    start_hour : u8,  // 24 hour time 
     stop_hour : u8,
-    min_sec : u8,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    shared_dir : Option<String>,
+    blacklist_times : Option<DateTimeConfig>,
+    event_handler : Option<EventHandlerConfig>,
+    recorder : RecorderConfig,
 }
 
 impl Config {
 
     // Temp default solution
-    pub async fn new() -> Result<Config, ConfigError> {
+    pub fn new() -> Result<Config, ConfigError> {
         let home = env::var("HOME")?;
         let mut config_file = String::new();
         
         File::open(home + DEFAULT_CONFIG)
-            .and_then(|mut f| f.read_to_string(config_file))?;
-        
-        let config : Config = toml::from_str(config_file)?; 
+            .and_then(|mut f| f.read_to_string(&mut config_file))?;
+       
+        let config : Config = toml::from_str(config_file.as_str())?; 
         Ok(config)
     }
        
-    pub fn shared_dir<'a>(&'a self) -> &'a String {
-        &self.shared_dir
+    pub fn shared_dir(&self) -> Result<String, ConfigError> {
+        let mut home_share = env::var("HOME")?;
+        home_share += DEFAULT_SHARE;
+        
+        let share_dir = self.shared_dir.clone().unwrap_or(home_share);
+
+        Ok(share_dir.to_owned())
     }
     
+    pub fn blacklists(&self) -> DateTimeConfig {
+        self.blacklist_times.clone().unwrap_or_default()
+    }
+    
+/*
     pub fn proc_names<'a>(&'a self) -> &'a Vec<String> {
         &self.proc_names
     }
@@ -74,5 +127,7 @@ impl Config {
     pub fn min_sec(&self) -> u8 {
         self.min_sec
     }
+*/
+
 }
 
